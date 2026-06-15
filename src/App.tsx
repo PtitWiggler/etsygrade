6e2914b061c1d4713670069c6fa8926cb7ac1296
Thomas Hennebert <thomashennebert@live.fr>
2026-06-15 14:56:36 +0200

    Milestone 0: scaffold Vite + React + Manifest v3

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <section id="center">
+        <div className="title">
+          <h1>EtsyGrade</h1>
+          <p>Hello EtsyGrade</p>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default App
